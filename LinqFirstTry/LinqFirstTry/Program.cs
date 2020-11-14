f2a6b72ebdd876c753b84df0916e7b665d486d4b
zudora <zudora2000@yahoo.com>
2020-11-14 15:48:33 -0800

    Removed header fields from request template and reformatted for WHO request

--- LinqFirstTry/LinqFirstTry/Program.cs
+++ LinqFirstTry/LinqFirstTry/Program.cs
@@ -5,13 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
-
+using Newtonsoft.Json;
 
 namespace LinqFirstTry {
-    public class Indicator {
+    public class IndicatorNode {
         public string IndicatorCode { get; set; }
-        public string IndicatorName { get; set; }      
+        public string IndicatorName { get; set; }
         public string Language { get; set; }
+    }
+    public class Root {
+        [JsonProperty("@odata.context")]
+        public string OdataContext { get; set; }
+        public List<IndicatorNode> value { get; set; }
     }
     class Program {
         static HttpClient client = new HttpClient();
@@ -20,29 +25,49 @@ namespace LinqFirstTry {
         }
         
         static async Task RunAsync() {
-            // Update port # in the following line.
+            string airPoll = "Ambient air pollution attributable deaths";
             string nameExact = "Reported number of people receiving antiretroviral therapy";
-            client.BaseAddress = new Uri("https://ghoapi.azureedge.net/api/Indicator?$filter=IndicatorName eq '"+ nameExact +"'/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.BaseAddress = new Uri("https://ghoapi.azureedge.net/api/Indicator?$filter=IndicatorName eq '"+ airPoll +"'");
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(
+            //    new MediaTypeWithQualityHeaderValue("application/json"));
 
             try {
                 //// Get the indicator id
-                Indicator indicator = await GetIndicatorAsync(client.BaseAddress);                
+                List<IndicatorNode> Indicators = await GetIndicatorAsync(client.BaseAddress);
+                List<string> indicatorIds = GetIndicatorIds(Indicators);
+                foreach(string id in indicatorIds) {
+
+                }
             }
             catch (Exception e) {
                 Console.WriteLine(e.Message);
             }
             Console.ReadLine();
         }
-        static async Task<Indicator> GetIndicatorAsync(Uri requestUri) {
-            Indicator Indicator = null;
+        static async Task<List<IndicatorNode>> GetIndicatorAsync(Uri requestUri) {
+            List<IndicatorNode> Indicators = null;
+            Root root = null;
             HttpResponseMessage response = await client.GetAsync(requestUri);
             if (response.IsSuccessStatusCode) {
-                Indicator = await response.Content.ReadAsAsync<Indicator>();
+                root = await response.Content.ReadAsAsync<Root>();
+                
+                Indicators = root.value;
+                //string str = await response.Content.ReadAsStringAsync();
             }
-            return Indicator;
+            return Indicators;
+        }
+        static List<string> GetIndicatorIds(List<IndicatorNode> nodeList) {
+            List<string> idList = new List<string>();
+            foreach (IndicatorNode node in nodeList) {
+                if (node.Language == "EN") {
+                    string id = node.IndicatorCode;
+                    if (null != id && !idList.Contains(id)) {
+                        idList.Add(id);
+                    }
+                }
+            }
+            return idList;
         }
     }
 }
